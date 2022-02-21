5b500a1fcf5cab002c87a6872d6b850121349d38
SeppPenner <HansM2013@gmx.de>
2022-02-21 16:36:52 +0100

    Updated build script.

--- PushPackage.bat
+++
@@ -1,3 +0,0 @@
-dotnet nuget push "src\PagerDutyClient\bin\Release\HaemmerElectronics.SeppPenner.PagerDutyClient.*.nupkg" -s "github" --skip-duplicate
-dotnet nuget push "src\PagerDutyClient\bin\Release\HaemmerElectronics.SeppPenner.PagerDutyClient.*.nupkg" -s "nuget.org" --skip-duplicate -k "%NUGET_API_KEY%"
-PAUSE
